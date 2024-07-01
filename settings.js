8e68ca6cc7888b7cc305b0b16621ae3fbfbb185a
pasyaganz <BlackJack01@gmail.com>
2024-07-01 21:53:11 +0700

    my-rest-api

---
+++ settings.js
@@ -0,0 +1,39 @@
+const fs = require('fs')
+
+global.creator = 'PasyaGanz'// yourname
+global.MONGO_DB_URI = "mongodb+srv://pasyaganz:pasya333@cluster0.d83xhyc.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
+global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
+global.your_email = "fresh0012345@gmail.com" //email
+global.email_password = "pasya123" //application password email
+global.limitCount = 50
+global.YUOR_PORT = 8000
+global.loghandler = {
+	noapikey:{
+		status: 403,
+        message: 'Input parameter apikey',
+        creator: `${creator}`,
+        result: "error"
+    },
+    error: {
+        status: 503,
+        message: 'Service Unavaible, Sedang dalam perbaikan',
+        creator: `${creator}`
+    },
+    apikey: {
+    	status: 403,
+    	message: 'Forbiden, Invalid apikey',
+    	creator: `${creator}`
+    },
+    noturl: {
+    	status: 403,
+    	message: 'Forbiden, Invlid url, masukkan parameter url',
+    	creator: `${creator}`,
+    }
+}
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(`Update'${__filename}'`)
+	delete require.cache[file]
+	require(file)
+})
